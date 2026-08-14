ab8df2bbce7ba7c1474a81f79074eabb1dfc3de5
Julia <bogatayadomo@gmail.com>
2026-08-14 14:28:17 +0300

    reactdom1-9

---
+++ src/components/CalendarPeriod.jsx
@@ -0,0 +1,12 @@
+const CalendarPeriod = () => {
+
+    return (
+   <div className="calendar__period">
+                        <p className="calendar__p date-end">
+                          Срок исполнения:{" "}
+                          <span className="date-control">09.09.23</span>
+                        </p>
+                      </div>
+    )
+}
+export default CalendarPeriod
