c12720da0ff4a82f50f8fb4acd862ace75a66f2c
Julia <bogatayadomo@gmail.com>
2026-08-28 18:57:02 +0300

    reactdom3-6

--- src/components/PopExit.jsx
+++ src/components/PopExit.jsx
@@ -1,27 +1,32 @@
-const PopExit = () => {
-    
-    return (
-          <div className="pop-exit" id="popExit">
-          <div className="pop-exit__container">
-            <div className="pop-exit__block">
-              <div className="pop-exit__ttl">
-                <h2>Выйти из аккаунта?</h2>
-              </div>
-              <form className="pop-exit__form" id="formExit" action="#">
-                <div className="pop-exit__form-group">
-                  <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                    <a href="modal/signin.html">Да, выйти</a>{" "}
-                  </button>
-                  <button className="pop-exit__exit-no _hover03" id="exitNo">
-                    <a href="main.html">Нет, остаться</a>{" "}
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+import React from "react";
+import {
+  PopExitContainer,
+  PopExitBlock,
+  PopExitTitle,
+  PopExitFormGroup,
+  PopExitYes,
+  PopExitNo,
+} from "./PopExit.styled";
 
-    )
-
-} 
-export default PopExit
+const PopExit = ({ onClose }) => {
+  return (
+    <PopExitContainer id="popExit">
+        <PopExitBlock>
+          <PopExitTitle>
+            Выйти из аккаунта?
+          </PopExitTitle>
+          <form className="pop-exit__form" id="formExit" action="#">
+            <PopExitFormGroup>
+              <PopExitYes id="exitYes">
+                <a href="modal/signin.html">Да, выйти</a>{" "}
+              </PopExitYes>
+              <PopExitNo id="exitNo" onClick ={onClose}>
+                <a href="main.html">Нет, остаться</a>{" "}
+              </PopExitNo>
+            </PopExitFormGroup>
+          </form>
+        </PopExitBlock>
+    </PopExitContainer>
+  );
+};
+export default PopExit;
